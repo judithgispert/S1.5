c375aadede90297ae1ee673280a157d53332efb8
judithgispert <judithgispert@10.63.0.63>
2023-10-13 14:18:56 +0200

    collections.sort

---
+++ src/N1Ex1/N1Ex1.java
@@ -0,0 +1,19 @@
+package N1Ex1;
+
+import java.util.Collections;
+
+public class N1Ex1 {
+	
+	public static void main (String[]args) {
+		
+		List nameList = new List();
+		
+		Collections.sort(nameList.fullList());
+		
+		for(String name : nameList.getContentList()) {
+			System.out.println(name);
+		}
+		
+	}
+
+}
