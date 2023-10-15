44607815e13cb36c7cd9520d9473afc37d9674d3
judithgispert <judithgispert@10.63.0.63>
2023-10-15 12:18:58 +0200

    List directory + orden

--- src/N1Ex1/N1Ex1.java
+++ src/N1Ex1/N1Ex1.java
@@ -1,15 +1,35 @@
 package N1Ex1;
 
-import java.util.Collections;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class N1Ex1 {
 	
 	public static void main (String[]args) {
 		
-		List nameList = new List();
+		//Path path = Paths.get("names");
 		
-		Collections.sort(nameList.fullList());
+		File archivo = new File ("names");
+		System.out.println(archivo.getAbsolutePath());
+		System.out.println(archivo.exists());
+		
+		String[]name=archivo.list();
+		Arrays.sort(name);
+		System.out.println();
+		
+		for(int i=0;i<name.length;i++) {
+			System.out.println(name[i]);
+		}
+		
+		//ListDirectory directoryList = new ListDirectory(path);
+		
+		//for(Path pathDirectory : directoryList.getDirectoryList()) {
+	//		System.out.println(pathDirectory);
+		//}
 		
 	}
+		
 
 }
